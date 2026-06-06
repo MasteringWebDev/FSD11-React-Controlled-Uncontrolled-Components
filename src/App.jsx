f602ad74ccb93d6c3a33cc1f8e01fd3601e9fa84
AnkitKumarMishra5 <ankit5kumar14@gmail.com>
2026-06-06 22:17:26 +0530

    React: Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>Controlled & Uncontrolled Components</h1>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam ullam reprehenderit, sequi blanditiis autem voluptatem? Laboriosam dolore incidunt suscipit, id voluptatem at doloremque atque doloribus facilis consequatur ipsa autem ducimus.</p>
+    </>
+  )
+}
+
+export default App
